473d6e4e8f5ff5b38b40440d13a14abeb1ffc8ca
xuxingci <xuxingci@ata.net.cn>
2015-04-13 18:56:54 +0800

    添加依赖库，最简单例子

--- myblog/runserver.py
+++ myblog/runserver.py
@@ -1,1 +1,8 @@
 from flask import Flask
+
+
+if __name__ == "__main__":
+    # To allow aptana to receive errors, set use_debugger=False
+    app = Flask(__name__)
+    app.debug = True
+    app.run(host='0.0.0.0')
